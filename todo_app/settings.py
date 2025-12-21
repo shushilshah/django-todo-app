cc51416edd1df7eef4db248ebb302878b3e16ad8
ShushilShah <shahshushil3@gmail.com>
2025-12-21 10:12:10 +0545

    debugging enable

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%kwv74u-!)0yw-lvy=hr5@1@+z3b&=cx)wz%xk*lo8f)h2jqmx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
