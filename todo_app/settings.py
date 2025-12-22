8a6025f15af4691c13ae116801071a0471f2a67b
ShushilShah <shahshushil3@gmail.com>
2025-12-22 13:55:21 +0545

    debugg

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = "todo_app.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
     )
